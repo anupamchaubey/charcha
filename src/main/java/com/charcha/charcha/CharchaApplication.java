42e23bdc1b819bcae9364aebce07a9a578e531b3
lostannu <icyanupam@gmail.com>
2025-07-19 14:00:08 +0530

    working on a new project: Charcha

---
+++ src/main/java/com/charcha/charcha/CharchaApplication.java
@@ -0,0 +1,13 @@
+package com.charcha.charcha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CharchaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CharchaApplication.class, args);
+	}
+
+}
